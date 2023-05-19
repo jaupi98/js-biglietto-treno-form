9c68c4b45b90dfba5d992a3f79ac84a256036bde
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-05-19 16:32:27 +0200

    seconso

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+let button = document.getElementById('calculate');
+
+button.addEventListener('click' , function() {
+    let km = document.getElementById('km').value;
+    let age = document.getElementById('age').value;
+    
+
+});
