c52d1ec8da014cc2e997ec2ea0b3f666899b6a92
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-05-19 17:08:50 +0200

    terzo

--- js/script.js
+++ js/script.js
@@ -3,8 +3,23 @@
 let button = document.getElementById('calculate');
 
 button.addEventListener('click' , function() {
-    let km = document.getElementById('km').value;
-    let age = document.getElementById('age').value;
+    let km = parseInt( document.getElementById('km').value);
+    let age = parseInt( document.getElementById('age').value);
+    let price = km * 0.21;
+    let message = '';
     
+    if( age === 0){
+        console.log(price);
+        message += `il prezzo del biglietto e di : ${price}$`
+    }
+    else if( age === 1){
+        price *= 0.8;
+        message += `il prezzo del biglietto e scontaot del 20% di : ${price}$`
+    }
+    else{
+        price *= 0.6;
+        message += `il prezzo del biglietto e scontato del 40% di : ${price}$`
 
+    }
+    document.getElementById('price').innerText = message;
 });
